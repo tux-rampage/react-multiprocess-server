705cb56e3d991621c40592c65605ffd4388419be
tux-rampage <tux-rampage@users.noreply.github.com>
2018-01-24 06:51:05 +0100

    Add pcntl class

---
+++ src/ProcessControl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author      Axel Helmert <tuxrampage>
+ * @license     LGPL https://www.gnu.org/licenses/lgpl.txt
+ * @copyright   (c) 2018 Axel Helmert
+ */
+
+namespace React\MultiProcess;
+
+/**
+ * Abstracts PCNTL to make it's calls testable
+ *
+ * @ignoreCoverage
+ */
+class ProcessControl
+{
+    /**
+     * Forks the current process
+     *
+     * @return int 0 when this is the child, >0 in the parent process, -1 on failures
+     */
+    public function fork(): int
+    {
+        return \pcntl_fork();
+    }
+}
